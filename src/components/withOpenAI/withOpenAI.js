60fe915b119fd5b5ded9bf4c4be60dcf528a1c95
noramichalski <michalski.nora@gmail.com>
2023-02-28 17:08:20 -1000

    reverting query

--- src/components/withOpenAI/withOpenAI.js
+++ src/components/withOpenAI/withOpenAI.js
@@ -1,57 +1,59 @@
-import './withOpenAI.css';
-
 import { Configuration, OpenAIApi } from 'openai';
 import { useState } from 'react';
+import { useMutation } from '@tanstack/react-query';
 
+const withOpenAI = (WrappedComponent, apiModel, apiFunction) => {
 
-const withOpenAI = (WrappedComponent, apiModel, apiFunction, inputLabel) => {
-	
-	const configuration = new Configuration({
-		apiKey: process.env.REACT_APP_OPENAI_API_KEY
-	});
-	const openai = new OpenAIApi(configuration);
+  const configuration = new Configuration({
+    apiKey: process.env.REACT_APP_OPENAI_API_KEY
+  });
+  const openai = new OpenAIApi(configuration);
 
-	return () => {
-		const [prompt, setPrompt] = useState("");
-		const [output, setOutput] = useState("");
-		const [isLoading, setIsLoading] = useState(false);
+  const Component = () => {
+    const [prompt, setPrompt] = useState("");
+    const [output, setOutput] = useState("");
 
-		const handleInputChange = (value) => {
-			setPrompt(value);
-		};
+    const { mutate, isLoading, error } = useMutation(
+      async (prompt) => {
+        const response = await apiFunction(openai, prompt);
 
-		const executeAPI = async (e) => {
-			e.preventDefault();
-			setIsLoading(true);
+        if (apiFunction.toString().includes("createImage")) {
+          return response.data.data[0].url;
+        } else if (apiFunction.toString().includes("createCompletion")) {
+          return response.data.choices[0].text;
+        }
+      },
+      {
+        onSuccess: setOutput,
+        onError: console.error,
+        retry: 3, // Optional: Retrying the request up to 3 times if it fails
+        retryDelay: (attempt) => attempt * 1000 // Optional: Adding delay between retries
+      }
+    );
 
-			try {
-				const response = await apiFunction(openai, prompt);
+    const handleInputChange = (value) => {
+      setPrompt(value);
+    };
 
-				if (apiFunction.toString().includes("createImage")) {
-					setOutput(response.data.data[0].url);
-				} else if (apiFunction.toString().includes("createCompletion")) {
-					setOutput(response.data.choices[0].text);
-				}
+    const executeAPI = async (e) => {
+      e.preventDefault();
+      mutate(prompt);
+    };
 
-			} catch (error) {
-				console.error(error);
-			}
+    return (
+      <WrappedComponent
+        handleInputChange={handleInputChange}
+        executeAPI={executeAPI}
+        prompt={prompt}
+        output={output}
+        isLoading={isLoading}
+        apiModel={apiModel}
+        error={error}
+      />
+    );
+  };
 
-			setIsLoading(false);
-		};
-
-		return (
-			<WrappedComponent
-				handleInputChange={handleInputChange}
-				executeAPI={executeAPI}
-				prompt={prompt}
-				output={output}
-				isLoading={isLoading}
-				inputLabel={inputLabel}
-				apiModel={apiModel}
-			/>
-		);
-	};
+  return Component;
 };
 
 export default withOpenAI;
